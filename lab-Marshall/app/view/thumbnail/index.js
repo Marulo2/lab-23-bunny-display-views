b43b7f5f1b49852efe48f97e1684df9b7f64ff2b
Mars <marshallmiller92@gmail.com>
2016-08-23 11:49:44 -0700

    debugging

--- lab-Marshall/app/view/thumbnail/index.js
+++ lab-Marshall/app/view/thumbnail/index.js
@@ -5,6 +5,6 @@ const demoApp = angular.module('demoApp');
 
 demoApp.controller('ThumbnailController', ['$rootScope', ThumbnailController]);
 
-function ThumbnailController(){
+function ThumbnailController($rootScope){
   this.images = $rootScope.imageData;
 }
